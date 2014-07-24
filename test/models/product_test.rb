dc22fa4f45a18c816dac511b95484c55faa85f4d
Stephen Saunders <saunders.stephen@gmail.com>
2014-07-24 14:05:52 -0700

    added test fixtures and verified only unique titles are saved to database.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
   test "product attributes cannot be empty" do
     product = Product.new
     assert product.invalid?
@@ -26,5 +28,33 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title:            'My Book Title',
+                description:      'yo yo yo yogo',
+                price:            1,
+                image_url:        image_url)
+  end
+  test "image url is valid file type" do
+    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif/more}
+    
+    ok.each do |filename|
+      assert new_product(filename).valid?, "#{filename} should be valid"
+    end
+
+    bad.each do |filename|
+      assert new_product(filename).invalid?, "#{filename} should be invalid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "abc.jpg")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 end
