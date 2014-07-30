7cb455848d5606b4b7cbf00ac82355d7fc704112
Stephen Saunders <saunders.stephen@gmail.com>
2014-07-29 22:13:53 -0700

    add session counter to increment visits to store catalog

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
+    @count = store_visit_incrementer
     @products = Product.order(:title)
+  end
+
+  private
+
+  def store_visit_incrementer
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
